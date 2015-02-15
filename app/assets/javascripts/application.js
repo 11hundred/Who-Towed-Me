177403f1ad0e3374b8e7ca8d4f86fc5402be57cf
David Handschuh <david@11hundred.com>
2015-02-15 02:51:51 -0600

    revised mobile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,11 +20,22 @@
 $(document).ready(function() {
 
 	var windowHeight = $(window).height();   
+	var windowHeightHalf = $(window).height()/2;   
 	var windowWidth = $(window).width();   
 
 	// Set  viewport 
 	$('.sign').css({height:windowHeight -80});
 	$('.sign').css({width:windowWidth -80});
+	$('.sign .title').css({paddingTop:windowHeightHalf -100});
+
+
+	$('.search_button').click( function() {
+	    $('#plate_search').addClass("active");	
+	    $('#plate_search').focus();	
+	    $('.title h1').addClass("active");	
+	});
+
+
 })
 
 $(window).resize(function() {
